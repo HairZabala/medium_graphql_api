562d51dcb4772ca54af9a08d0f9a5a2a160a335f
Hair Zabala <hair.zabala@gmail.com>
2022-07-14 21:29:21 +1000

    Implement user model validacion

--- lib/medium_graphql_api/accounts/user.ex
+++ lib/medium_graphql_api/accounts/user.ex
@@ -3,11 +3,13 @@ defmodule MediumGraphqlApi.Accounts.User do
   import Ecto.Changeset
 
   schema "users" do
-    field :email, :string
-    field :firt_name, :string
-    field :last_name, :string
-    field :password_hash, :string
-    field :role, :string
+    field(:email, :string)
+    field(:firt_name, :string)
+    field(:last_name, :string)
+    field(:password_hash, :string)
+    field(:password, :string, virtual: true)
+    field(:password_confirmation, :string, virtual: true)
+    field(:role, :string, default: "user")
 
     timestamps()
   end
@@ -15,7 +17,19 @@ defmodule MediumGraphqlApi.Accounts.User do
   @doc false
   def changeset(user, attrs) do
     user
-    |> cast(attrs, [:firt_name, :last_name, :email, :password_hash, :role])
-    |> validate_required([:firt_name, :last_name, :email, :password_hash, :role])
+    |> cast(attrs, [:firt_name, :last_name, :email, :password, :password_confirmation, :role])
+    |> validate_required([:firt_name, :last_name, :email, :password, :password_confirmation, :role])
+    |> validate_format(:email, ~r/@/)
+    |> update_change(:email, &String.downcase(&1))
+    |> validate_length(:password, min: 6, max: 100)
+    |> validate_confirmation(:password)
+    |> unique_constraint(:email, message: "email_is_already_in_use")
+    |> hash_password
+
   end
+
+  defp hash_password(changeset) do
+    changeset
+  end
+
 end
